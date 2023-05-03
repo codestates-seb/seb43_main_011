a1756af277ca9efc45328af02dd757b12c9f9f4e
최재영 <107467750+220SRS@users.noreply.github.com>
2023-05-03 14:33:44 +0900

    Add files via upload

---
+++ be/cocktail/src/main/java/com/BE/cocktail/CocktailApplication.java
@@ -0,0 +1,13 @@
+package com.BE.cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailApplication.class, args);
+	}
+
+}
---
+++ be/cocktail/src/test/java/com/BE/cocktail/CocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BE.cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
