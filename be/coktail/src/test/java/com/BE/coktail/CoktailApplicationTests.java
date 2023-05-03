d648a8ce91034a2efe3ae8b4d144e2adb3555202
최재영 <107467750+220SRS@users.noreply.github.com>
2023-05-03 14:20:48 +0900

    Add files via upload

---
+++ be/coktail/src/test/java/com/BE/coktail/CoktailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BE.coktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
