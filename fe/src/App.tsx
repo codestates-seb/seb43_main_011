03963483bd9d794f1d5072c776edd054d2f0562f
rosevilleage <wkdcks125@naver.com>
2023-05-04 20:32:38 +0900

    Feat: SideBar 컴포넌트 생성

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,20 @@
-import { ThemeProvider } from 'styled-components';
-import theme from './components/style/theme';
+import { ThemeProvider } from "styled-components";
+import theme from "./components/style/theme";
+import styled from "styled-components";
+import GlobalStyle from "./components/style/GlobalStyles";
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App"></div>
-    </ThemeProvider>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Main></Main>
+      </ThemeProvider>
+    </>
   );
 }
 
