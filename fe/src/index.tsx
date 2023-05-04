90b7b35d5a7e7081819ebd9266a98f47ccd44e7d
rosevilleage <wkdcks125@naver.com>
2023-05-04 22:05:37 +0900

    Desing : components 비율 조정 - component들의 비율을 통일

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/header/Header";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -12,7 +11,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Header />
     </BrowserRouter>
   </React.StrictMode>,
 );
