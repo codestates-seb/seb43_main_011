bd4ca56ff4d45ee50fdfbcc70217c8a895b9259f
220SRS <jesikrjy@gmail.com>
2023-05-12 19:03:48 +0900

    Feat : 정규레시피 검색기능 구현
    - 정규레시피를 검색하면 페이징으로 응답

--- be/cocktail/src/main/java/com/BE/cocktail/persistence/repository/regularRecipe/RegularRecipeRepository.java
+++ be/cocktail/src/main/java/com/BE/cocktail/persistence/repository/regularRecipe/RegularRecipeRepository.java
@@ -1,12 +1,19 @@
 package com.BE.cocktail.persistence.repository.regularRecipe;
 
 
+import com.BE.cocktail.dto.regularRecipe.RegularSearchResponseDto;
 import com.BE.cocktail.persistence.domain.regularRecipe.RegularRecipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface RegularRecipeRepository extends JpaRepository<RegularRecipe, Long> {
 
     List<RegularRecipe> findAllByName(String name);
 
+    @Query("SELECT r FROM RegularRecipe r WHERE (r.name LIKE %:keyword% OR r.ingredient LIKE %:keyword%)")
+    Page<RegularRecipe> findAllByKeyword(String keyword, PageRequest id);
 }
--- be/cocktail/src/main/java/com/BE/cocktail/service/regularRecipe/RegularRecipeService.java
+++ be/cocktail/src/main/java/com/BE/cocktail/service/regularRecipe/RegularRecipeService.java
@@ -2,13 +2,21 @@ package com.BE.cocktail.service.regularRecipe;
 
 import com.BE.cocktail.dto.apiResponse.ApiResponse;
 import com.BE.cocktail.dto.apiResponse.CocktailRtnConsts;
+import com.BE.cocktail.dto.customRecipe.CustomSearchResponseDto;
 import com.BE.cocktail.dto.regularRecipe.RegularRecipeGetResponseDto;
 
 import com.BE.cocktail.dto.regularRecipe.RegularRecipeMultiResponseDto;
+import com.BE.cocktail.dto.regularRecipe.RegularSearchResponseDto;
+import com.BE.cocktail.dto.utils.MultiResponseDto;
+import com.BE.cocktail.dto.utils.PageInfo;
 import com.BE.cocktail.exception.CocktailException;
+import com.BE.cocktail.persistence.domain.customRecipe.CustomRecipe;
 import com.BE.cocktail.persistence.domain.regularRecipe.RegularRecipe;
 import com.BE.cocktail.persistence.repository.regularRecipe.RegularRecipeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,4 +42,11 @@ public class RegularRecipeService {
         return RegularRecipeMultiResponseDto.of(regularRecipes);
     }
 
+    public MultiResponseDto<RegularSearchResponseDto> searchPaging(String keyword, int page, int size) {
+
+        Page<RegularRecipe> pages = regularRecipeRepository.findAllByKeyword(keyword, PageRequest.of(page, size, Sort.by("id").descending()));
+        List<RegularRecipe> responses = pages.getContent();
+
+        return MultiResponseDto.of(RegularSearchResponseDto.listOf(responses), PageInfo.of(pages));
+    }
 }
