454639dc0964da85b02e89e6093341a51eddeea1
Heoyungyoung <gjdbs2597@naver.com>
2023-05-19 02:01:15 +0900

    Feat : Swagger 및 북마크 엔티티 작성
    - Swagger 및 북마크 엔티티 작성

--- be/cocktail/src/main/java/com/BE/cocktail/controller/member/MemberController.java
+++ be/cocktail/src/main/java/com/BE/cocktail/controller/member/MemberController.java
@@ -4,6 +4,8 @@ import com.BE.cocktail.dto.apiResponse.ApiResponse;
 import com.BE.cocktail.dto.member.MemberInfoResponseDto;
 import com.BE.cocktail.dto.member.SignUpDto;
 import com.BE.cocktail.service.member.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@Api(tags = "member",description = "유저 API")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/signup")
     public ApiResponse<Void> signUpMember(@Valid @RequestBody SignUpDto sign) {
 
@@ -26,6 +30,7 @@ public class MemberController {
         return ApiResponse.ok();
     }
 
+    @ApiOperation(value = "회원 상세 정보")
     @GetMapping("/member/myPage")
     public ApiResponse<MemberInfoResponseDto> findMypageInfo() {
 
--- be/cocktail/src/main/java/com/BE/cocktail/controller/regularRecipe/RegularRecipeController.java
+++ be/cocktail/src/main/java/com/BE/cocktail/controller/regularRecipe/RegularRecipeController.java
@@ -7,17 +7,20 @@ import com.BE.cocktail.dto.regularRecipe.RegularRecipeResponse;
 import com.BE.cocktail.dto.regularRecipe.RegularSearchResponseDto;
 import com.BE.cocktail.dto.utils.MultiResponseDto;
 import com.BE.cocktail.service.regularRecipe.RegularRecipeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "regularRecipe",description = "정규 레시피 API")
 @RequestMapping("/regular")
 public class RegularRecipeController {
 
     private final RegularRecipeService regularRecipeService;
 
-
+    @ApiOperation(value = "정규 레시피 상세 조회")
     @GetMapping("/find/{recipe_id}")
     public ApiResponse<RegularRecipeGetResponseDto> findDetailInfo(@PathVariable("recipe_id") Long id) {
 
@@ -26,6 +29,7 @@ public class RegularRecipeController {
         return ApiResponse.ok(response);
     }
 
+    @ApiOperation(value = "정규 레시피 검색")
     @GetMapping("/search/{keyword}")
     public ApiResponse<MultiResponseDto<RegularSearchResponseDto>> searchRegular(@PathVariable("keyword") String keyword,
                                                                                    @RequestParam int page,
@@ -36,6 +40,7 @@ public class RegularRecipeController {
         return ApiResponse.ok(responseDto);
     }
 
+    @ApiOperation(value = "정규 레시피 전체 조회")
     @GetMapping("/findAll/{alc_vol}")
     public ApiResponse<MultiResponseDto<RegularRecipeResponse>> findRecipes(@PathVariable("alc_vol") Integer alcVolRange,
                                                                             @RequestParam int page,
