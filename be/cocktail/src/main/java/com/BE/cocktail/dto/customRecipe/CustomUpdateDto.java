15c0da01886809ec159975af2159a1b6a12d1ec6
220SRS <jesikrjy@gmail.com>
2023-05-22 02:48:23 +0900

    멤버 tran 추가

--- be/cocktail/src/main/java/com/BE/cocktail/dto/customRecipe/CustomUpdateDto.java
+++ be/cocktail/src/main/java/com/BE/cocktail/dto/customRecipe/CustomUpdateDto.java
@@ -11,20 +11,16 @@ public class CustomUpdateDto {
 
 //    @Pattern(regexp = "^[가-힣]+$")
     @Size(min=3, max=255)
-    @NotBlank
     private String name;
 
 //    @Pattern(regexp = "^[가-힣a-zA-Z]+$")
     @Size(min=3, max=255)
-    @NotBlank
     private String description;
 
     @Size(max=900)
-    @NotBlank
     private String recipe;
 
 //    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$")
-    @NotBlank
     private String ingredient;
 
 }
--- be/cocktail/src/main/java/com/BE/cocktail/service/member/MemberService.java
+++ be/cocktail/src/main/java/com/BE/cocktail/service/member/MemberService.java
@@ -14,6 +14,7 @@ import com.BE.cocktail.utils.GetAuthUserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -59,6 +60,7 @@ public class MemberService {
         return MemberInfoResponseDto.of(member);
     }
 
+    @Transactional
     public void updateContent(MemberUpdateDto updateDto) {
 
         Member findMember = getLoginMember();
@@ -66,6 +68,7 @@ public class MemberService {
         findMember.updateContent(updateDto);
     }
 
+    @Transactional
     public void updateImage(MultipartFile image) throws IOException {
 
         String storedFileName = s3Uploader.upload(image,"images");
