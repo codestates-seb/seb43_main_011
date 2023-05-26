607ee1419f39f6ff97ebfbc58db527af15894ffe
rosevilleage <wkdcks125@naver.com>
2023-05-26 22:18:11 +0900

    Feat : 검색 결과 없을 시 안내문 등장

--- fe/src/pages/SearchResults.tsx
+++ fe/src/pages/SearchResults.tsx
@@ -8,6 +8,7 @@ import RecipePagination from "../components/card/RecipePagination";
 import { useSearchedPagination } from "../hooks/useSearchedPagination";
 import LoadingComponent from "../components/loading/LoadingComponent";
 import { useQueryClient } from "react-query";
+import doNotHave from "../images/doNothaveRecipe.png";
 
 const CardListArea = styled.div`
   margin: 30px 0;
@@ -16,6 +17,24 @@ const CardListArea = styled.div`
   grid-template-rows: repeat(4, 1fr);
   grid-gap: 30px;
   place-items: center;
+`;
+
+const NoneSearchedBox = styled.div`
+  width: 100%;
+  min-height: 70vh;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 3rem;
+  > p {
+    font-size: 1.5rem;
+  }
+`;
+
+const DoNotHaveImage = styled.img`
+  width: 400px;
 `;
 
 export default function SearchResults() {
@@ -29,11 +48,20 @@ export default function SearchResults() {
   }, []);
   const { data, isLoading, isPreviousData, hasMore, onNextClick, onPrevClick } =
     useSearchedPagination(path, searchValue);
+  const currentTab = path === "regular" ? "정규" : "커스텀";
+  const anotherTab = path === "regular" ? "커스텀" : "정규";
 
   return (
     <RecipesContainer>
       <SearchResultTab tabs={category} path={path} setPath={setPath} />
       {isLoading && <LoadingComponent />}
+      {data?.data.length === 0 && (
+        <NoneSearchedBox>
+          <DoNotHaveImage src={doNotHave} />
+          <h1>검색 결과에 해당하는 {currentTab} 레시피가 존재하지 않습니다.</h1>
+          <p>{anotherTab} 레시피를 확인해 보세요</p>
+        </NoneSearchedBox>
+      )}
       <CardListArea>
         {!isLoading &&
           data?.data.map((card, i) => {
