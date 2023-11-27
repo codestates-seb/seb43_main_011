f26a00d687535d3dc3e60a37389ff4a39c4a80c7
rosevilleage <a65083557@gmail.com>
2023-11-28 00:04:00 +0900

    refactor: invaildateQueries 제거

--- fe/src/hooks/useDeleteRecipe.ts
+++ fe/src/hooks/useDeleteRecipe.ts
@@ -1,22 +1,13 @@
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import { tokenInstance } from "../utils/tokeninstance";
 
 export const useDeleteRecipe = () => {
-  const queryClient = useQueryClient();
-
   const deleteRecipe = async (id: number) => {
     const response = await tokenInstance.delete(`/custom/delete/${id}`);
     return response.data;
   };
 
-  const deleteMutation = useMutation(deleteRecipe, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("myRecipe");
-    },
-    onError: () => {
-      queryClient.invalidateQueries("myRecipe");
-    },
-  });
+  const deleteMutation = useMutation(deleteRecipe);
 
   return deleteMutation;
 };
